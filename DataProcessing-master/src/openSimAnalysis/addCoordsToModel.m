649f11a95b7a6d424a787f83d1cf15c329f95b9b
Gavin Lenton <gavin.lenton@griffithuni.edu.au>
2016-10-17 14:00:44 +1000

    Added function to add coordinates to model

--- DataProcessing-master/src/openSimAnalysis/addCoordsToModel.m
+++ DataProcessing-master/src/openSimAnalysis/addCoordsToModel.m
@@ -1,6 +1,10 @@
 function addCoordsToModel(model_file)
 %Assign knee abd/add and int/ext rotation DOFS to an input model
-%   Input the model file, and directory to the session
+%   Input the directory to the model file.
+
+%   Get the coordinates for both knee joints and add adduction and int/ext rotation to the coordinate
+%   set. Then change the simmspline functions in the knee joint spatial transform 
+%   to linear functions and set the coefficients to 1 and 0
 
 % 11/10/2016 - Gavin Lenton
 
@@ -20,119 +24,82 @@ import org.opensim.modeling.*
 
 % Get model
 current_model = Model(model_file);
-state = current_model.initSystem();
-
-% updCoord = current_model.updCoordinateSet();
-
-% Create new coord set
-newCoordSet = CoordinateSet();
-axisValues = Vec3();
+% Create new model
+new_model = Model(current_model);
+new_model.setName('FBM_openKnee');
+new_model.initSystem;
 
 % Get the model coordinate set
-modelCoordSet = current_model.getCoordinateSet();
-kneeJoint = CustomJoint.safeDownCast(current_model.getJointSet().get('knee_r'));
-kneeCoordinateSet = kneeJoint.getCoordinateSet();
+modelJointSet = new_model.getJointSet();
 
-kneeJoint.append_CoordinateSet(coord2);
+sides = {'r', 'l'};
 
-kneeCoordinateSet.get(0).setRange(rotRangeAbd);
-kneeCoordinateSet.set(1).setName;
-kneeCoordinateSet.set(2).setRange(rotRangeAbd);
-
-
-kneeCoord = modelCoordSet.get('knee_angle_r');
-joint = kneeCoord.getJoint;
-eval(['concreteJoint = ' char(joint.getConcreteClassName) '.safeDownCast(joint);'])
-sptr = concreteJoint.getSpatialTransform; 
-
-for ip = 0 : 5
-     if strcmp(char(sptr.getCoordinateNames().get(ip)), char(kneeCoord.getName))
-          sptr.getTransformAxis(ip).getAxis(axisValues);
-          break
+% Loop through both sides
+for i = 1:2
+     
+     % Get knee joint
+     kneeJointR = CustomJoint.safeDownCast(new_model.getJointSet().get(['knee_', sides{i}]));
+     kneeCoordSet = kneeJointR.getCoordinateSet();
+     
+     if i == 1
+          % Specify range for joints
+          rotRangeAbd = [-0.349065, 0.349065];
+          rotRangeIntRot = [-0.087 0.349065];
+          
+     else
+          rotRangeAbd = [-0.349065, 0.349065];
+          rotRangeIntRot = [-0.349065 0.087];
      end
+     
+     % Update coordinate sets
+     flexCoord = kneeCoordSet.get(0);
+     
+     % Create clone of knee_angle_r for other DOFs
+     adductionCoord = flexCoord.clone();
+     intRotationCoord = flexCoord.clone();
+     
+     % Update name and range for other DOF
+     adductionCoord.setName(['knee_adduction_', sides{i}]); adductionCoord.setRange(rotRangeAbd);
+     intRotationCoord.setName(['knee_rotation_', sides{i}]); intRotationCoord.setRange(rotRangeIntRot);
+     
+     % Append coordinates to coordinate set
+     kneeCoordSet.cloneAndAppend(adductionCoord);
+     kneeCoordSet.cloneAndAppend(intRotationCoord);
+     
+     % Add coordinate set to joint
+     kneeJointR.set_CoordinateSet(kneeCoordSet.clone());
+     
+     % Add joint to joint set
+     modelJointSet.cloneAndAppend(kneeJointR);
+     
+     %% Update spatial transforms
+     transformKnee = kneeJointR.getSpatialTransform();
+     
+     % Get transform for rotation axes 2 and 3
+     knee_add = transformKnee.getTransformAxis(1);
+     knee_rot = transformKnee.getTransformAxis(2);
+     
+     % Set coordinate names to match coordinates
+     knee_add.set_coordinates(0, ['knee_adduction_', sides{i}]);
+     knee_rot.set_coordinates(0, ['knee_rotation_', sides{i}]);
+     
+     % Create the linear function with coefficients [1,0]
+     linFunction = LinearFunction();
+     functionCoeefficients = ArrayDouble();
+     functionCoeefficients.setValues([1,0], 2)
+     linFunction.setCoefficients(functionCoeefficients);
+     
+     % Set function to the transform
+     knee_add.setFunction(linFunction);
+     knee_rot.setFunction(linFunction);
+     
+     % Add transforms back to model for rotations 2 and 3 (non flexion
+     % rotations)
+     transformKnee.set_rotation2(knee_add.clone());
+     transformKnee.set_rotation3(knee_rot.clone());
+     
 end
 
+% Print the new model
+new_model.print(regexprep(model_file, 'FBM','openKnee'));
 
-% Get knee joint
-kneeJoint = CustomJoint.safeDownCast(current_model.getJointSet().get('knee_r'));
-
-% Get spatial transform
-transformKnee = kneeJoint.getSpatialTransform();
-
-% Get transform for rotation axes 2 and 3
-A = transformKnee.getTransformAxis(1);
-B = transformKnee.getTransformAxis(2);
-
-% Set coordinate names to match coordinates
-A.set_coordinates(0, 'knee_adduction_r');
-B.set_coordinates(0, 'knee_rotation_r');
-
-% Create the linear function with coefficients [1,0]
-linFunction = LinearFunction();
-functionCoeefficients = ArrayDouble();
-functionCoeefficients.setValues([1,0], 2)
-linFunction.setCoefficients(functionCoeefficients);
-
-% Set function to the transform
-A.setFunction(linFunction);
-B.setFunction(linFunction);
-
-% Specify range for joints
-rotRangeAbd = [-1.0943951, 1.0943951];
-rotRangeIntRot = [-0.087 0.349065];
-
-% Update coordinate sets
-kneeJoint = CustomJoint.safeDownCast(current_model.getJointSet().get('knee_r'));
-kneeCoordSet = kneeJoint.getCoordinateSet();
-flexCoord = kneeCoordSet.get(0);
-% Create clone of knee_angle_r for other DOFs
-adductionCoord = flexCoord.clone();
-intRotationCoord = flexCoord.clone();
-
-% Update name and range for other DOF
-adductionCoord.setName('knee_adduction_r'); adductionCoord.setRange(rotRangeAbd);
-intRotationCoord.setName('knee_rotation_r'); intRotationCoord.setRange(rotRangeIntRot);
-
-kneeJoint.set_CoordinateSet(adductionCoord)
-kneeJoint.upd_CoordinateSet(adductionCoord);
-kneeJoint.upd_CoordinateSet().set(adductionCoord);
-
-% Get SimmSpline values
-% Abd/add
-jointFuncR2 = transformKnee.get_rotation2().getFunction();
-simmsplineR2 = SimmSpline.safeDownCast(jointFuncR2);
-
-% Int/ext rotation
-jointFuncR3 = transformKnee.get_rotation3().getFunction();
-simmsplineR3 = SimmSpline.safeDownCast(jointFuncR3);
-
-% Get X and Y values - only need one X value because they are both knee
-% flexion
-simmsplineX2 = simmsplineR2.getX;
-simmsplineY2 = simmsplineR2.getY;
-simmsplineRY3 = simmsplineR3.getY;
-
-% Determine number of values in spline
-splineLength = simmsplineX2.getSize();
-% Get the values from the Vec3
-splineValuesAbd = zeros(1, splineLength);
-splineValuesRot = zeros(1, splineLength);
-splineValuesFlex = zeros(1, splineLength);
-
-% Manually defined the spline values because I couldn't get length
-% -1 when accessing OpenSim values because it starts at 0
-for splineValue = 1:splineLength
-     splineValuesFlex(1, splineValue) = simmsplineX2.get(splineValue-1);
-     splineValuesAbd(1, splineValue) = simmsplineY2.get(splineValue-1);
-     splineValuesRot(1, splineValue) = simmsplineRY3.get(splineValue-1);
-end
-
-% Rotation for left leg is opposite sign.
-splineValuesRot2 = splineValuesRot .* -1;
-
-% Initialize model
-osimModel= current_model;
-
-% Get the model coordinate name from the new model for knee ab/add and
-% int/ext rot
-modelCoordSet = osimModel.getCoordinateSet(); 
